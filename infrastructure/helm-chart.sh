837842f950e58ae6c3034116bd4ae1ca7ea54a26
usuladams <usul.adem@gmail.com>
2025-07-07 12:55:31 +0000

    added qa automation pipeline for dev

--- infrastructure/helm-chart.sh
+++ infrastructure/helm-chart.sh
@@ -2,6 +2,9 @@ helm package petclinic_chart/
 helm push my-chart-0.1.0.tgz oci://LOCATION-docker.pkg.dev/PROJECT/REPOSITORY
 gcloud artifacts docker images list europe-west3-docker.pkg.dev/k8s-demo-464210/petclinic-helm-charts
 
+helm push petclinic_chart-0.0.1.tgz oci://europe-west3-docker.pkg.dev/k8s-demo-464210/petclinic-helm-charts
+
+
 
 helm upgrade --install \
     petclinic-app-release oci://${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${HELM_CHART_REPO_NAME}/petclinic_chart \
