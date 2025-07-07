09d6835b888e9f9a7ec657229fc7ee94cfc9954b
usuladams <usul.adem@gmail.com>
2025-07-07 10:42:52 +0000

    added scripts for running dummy selenium job

--- infrastructure/helm-chart.sh
+++ infrastructure/helm-chart.sh
@@ -1,3 +1,18 @@
 helm package petclinic_chart/ 
 helm push my-chart-0.1.0.tgz oci://LOCATION-docker.pkg.dev/PROJECT/REPOSITORY
 gcloud artifacts docker images list europe-west3-docker.pkg.dev/k8s-demo-464210/petclinic-helm-charts
+
+
+helm upgrade --install \
+    petclinic-app-release oci://${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${HELM_CHART_REPO_NAME}/petclinic_chart \
+    --version ${BUILD_NUMBER} --namespace petclinic-dev
+
+
+
+"${LOCATION}-docker.pkg.dev/${PROJECT_ID}/${APP_REPO_NAME}"
+
+
+
+helm upgrade --install \
+        petclinic-app-release  --version ${BUILD_NUMBER} \
+        --namespace petclinic-dev
