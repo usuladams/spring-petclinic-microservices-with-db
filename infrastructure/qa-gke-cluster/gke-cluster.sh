c3acd19101eb919b68c98fcbd720237ad46fcfb2
usuladams <usul.adem@gmail.com>
2025-07-08 13:01:34 +0000

    added cluster.yaml file

---
+++ infrastructure/qa-gke-cluster/gke-cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CLUSTER_NAME="zeus-cluster"
+REGION="us-west3"
+ZONE="us-west3-c"
+NUM_NODES=2
+DISK_SIZE="30GB"
+MACHINE_TYPE="e2-medium"
+
+echo "Creating GKE cluster: $CLUSTER_NAME in $REGION ($ZONE)..."
+
+gcloud container clusters create $CLUSTER_NAME \
+    --location $REGION \
+    --node-locations $ZONE \
+    --machine-type $MACHINE_TYPE \
+    --disk-size $DISK_SIZE \
+    --num-nodes $NUM_NODES
+
+echo "Cluster $CLUSTER_NAME creation complete!"
