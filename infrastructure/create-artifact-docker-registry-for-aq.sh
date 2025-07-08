bef27f5913b3dfee1ccaae87b286fb4e68119075
usuladams <usul.adem@gmail.com>
2025-07-08 15:07:24 +0000

    added jenkinsfile petclinic-weekly-qa for release branch

--- infrastructure/create-artifact-docker-registry-for-aq.sh
+++ infrastructure/create-artifact-docker-registry-for-aq.sh
@@ -1,5 +1,6 @@
 APP_REPO_NAME="petclinic-app-qa"
 LOCATION="europe-west3"
+HELM_CHART_REPO_NAME="petclinic-helm-charts-qa"
 
 gcloud artifacts repositories describe ${APP_REPO_NAME} --location=${LOCATION} > /dev/null 2>&1 \
             && echo "Artifact Repo '${APP_REPO_NAME}' already exists." \
@@ -8,6 +9,17 @@ gcloud artifacts repositories describe ${APP_REPO_NAME} --location=${LOCATION} >
               --repository-format=docker \
               --location="${LOCATION}" \
               --description="GCP Repo for petclinic app" \
+              --immutable-tags \
+              --async \
+              --disable-vulnerability-scanning)
+
+gcloud artifacts repositories describe ${HELM_CHART_REPO_NAME} --location=${LOCATION} > /dev/null 2>&1 \
+            && echo "Artifact Repo '${HELM_CHART_REPO_NAME}' already exists." \
+            || (echo "Creating Artifact Repo '${HELM_CHART_REPO_NAME}'..." && \
+            gcloud artifacts repositories create ${HELM_CHART_REPO_NAME} \
+              --repository-format=docker \
+              --location="${LOCATION}" \
+              --description="Helm Chart Repo for petclinic app" \
               --immutable-tags \
               --async \
               --disable-vulnerability-scanning)
